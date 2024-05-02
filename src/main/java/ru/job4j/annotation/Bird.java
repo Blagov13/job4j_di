2183f5550065ecf2c8971cd13d6c8b2a1bdec183
aleksandrblagov <blagov13rus@gmail.com>
2024-05-02 16:46:32 +0300

    Commit

---
+++ src/main/java/ru/job4j/annotation/Bird.java
@@ -0,0 +1,13 @@
+package ru.job4j.annotation;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Bird implements Pet<Bird> {
+    private String name = "Bird";
+
+    @Override
+    public String print() {
+        return "Bird name = %s".formatted(name);
+    }
+}
