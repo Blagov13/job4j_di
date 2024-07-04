f254f2950dfc2117d4a5aa9de6849e56245538f3
aleksandrblagov <blagov13rus@gmail.com>
2024-07-05 00:41:49 +0300

    3. Dockerfile + maven [#505041]

--- src/main/java/ru/job4j/Main.java
+++ src/main/java/ru/job4j/Main.java
@@ -1,7 +1,8 @@
-package ru.job4j.annotation;
+package ru.job4j;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import ru.job4j.annotation.Man;
 
 public class Main {
     public static void main(String[] args) {
